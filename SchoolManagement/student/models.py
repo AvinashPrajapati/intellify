89b6de3d354553fb111f6f7a1a6c5e46fdd74de7
*avinashprajapati <62180007+AvinashPrajapati@users.noreply.github.com>
2022-08-09 18:17:21 +0530

    commets added in models.py in student app

--- SchoolManagement/student/models.py
+++ SchoolManagement/student/models.py
@@ -2,22 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 from faculty.models import Teacher, Class, CustomUser
 # Create your models here.
+
+#student model inheriting faculty's CustomUser model which is the current Auth model
 class Student(models.Model):
-    student = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    student = models.OneToOneField(CustomUser, on_delete=models.CASCADE)  # one to one field because one user -> will be teacher or student.
     dob = models.DateField(editable=True, null=True)
     contact_number = models.IntegerField()
     standard = models.CharField(max_length=3)
     stream = models.CharField(max_length=20, default="none")
     class_section = models.CharField(max_length=20, default='A')
     roll_number = models.IntegerField(null=True, blank=True)
-    classes_taught = models.ManyToManyField(Class)
+    classes_taught = models.ForeignKey(Class)  # ForeignKey field because one student has many classes
 
-    # address onetoonefield relation
-    # one bank_account_no field
+    # address onetoonefield relation can be added
+    # one bank_account_no field can also be added
 
     def __str__(self):
         return self.student.username
 
+# in case if grade system required further
 class Grade(models.Model):
     # considering id as assentment id and as primary key
     student_name = models.OneToOneField(Student, on_delete=models.CASCADE) 
