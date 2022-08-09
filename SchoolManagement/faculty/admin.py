0973480b9fd42bc8ab3a72d92d6661b47eed7f29
*avinashprajapati <62180007+AvinashPrajapati@users.noreply.github.com>
2022-08-09 18:06:37 +0530

    comments added in admin.py in faculty app

--- SchoolManagement/faculty/admin.py
+++ SchoolManagement/faculty/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
+# Registering faculty app models here.
+# list_display is used to display table field name in admin panel
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username','id','member_type', 'first_name', 'last_name', 'email', 'is_superuser')
