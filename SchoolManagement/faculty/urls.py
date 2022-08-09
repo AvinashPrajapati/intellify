79d2affb8bf7cb2f4923ffad54ba676ab4a72795
*avinashprajapati <62180007+AvinashPrajapati@users.noreply.github.com>
2022-08-09 18:04:42 +0530

    comments in urls.py in faculty app

--- SchoolManagement/faculty/urls.py
+++ SchoolManagement/faculty/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import * 
 
+# url paths for teacher actions
 urlpatterns = [
     path('', teacherProfile, name='teacherprofile'),
     path('teacher-register/', teacherRegister, name='teacherregister'),
